8730e8044d2db2c9091b923ed613a44f6b65ed6c
Bruno Corrêa <brunosantos6ft@gmail.com>
2024-01-19 09:18:06 -0300

    chore: Add some tests for business rules + some todos

--- src/__tests__/Entities/producer.spec.ts
+++ src/__tests__/Entities/producer.spec.ts
@@ -13,7 +13,7 @@ describe('Producer Entity', () => {
 
   afterAll(async () => await app.close())
 
-  it('should create a producer', async () => {
+  it('should be able to create a producer', async () => {
     // Arrange
     const producerMockData = generateProducer()
     const producerCropMockData = Array.from(
@@ -34,4 +34,83 @@ describe('Producer Entity', () => {
     // Assert
     expect(producerInstance).toBeDefined()
   })
+
+  it('should create a producer crops', async () => {
+    // Arrange
+    const producerMockData = generateProducer()
+    const producerCropMockData = Array.from(
+      { length: faker.number.int({ min: 1, max: 5 }) },
+      generateCrop,
+    )
+
+    // Act
+    const producerCropInstance = producerCropMockData.map(
+      (crop) => new ProducerCrop(crop),
+    )
+    const producerInstance = new Producer({
+      ...producerMockData,
+      producerCrops: producerCropInstance,
+    })
+
+    // Assert
+    expect(producerInstance.producerCrops).toEqual(producerCropMockData)
+  })
+
+  it('should throw an error for invalid CPF/CNPJ', () => {
+    // Arrange
+    const producerMockData = generateProducer()
+    const producerCropMockData = Array.from(
+      { length: faker.number.int({ min: 1, max: 5 }) },
+      generateCrop,
+    )
+    const invalidCpfCnpj = '123'
+
+    // Act
+    const producerCropInstance = producerCropMockData.map(
+      (crop) => new ProducerCrop(crop),
+    )
+
+    const producerInstance = new Producer({
+      ...producerMockData,
+      producerCrops: producerCropInstance,
+      cpfCnpj: invalidCpfCnpj,
+    })
+
+    // Assert
+    expect(producerInstance).toThrowError()
+  })
+
+  it('should throw an error if cultivable area + vegetation area > total area', () => {
+    // Arrange
+    const producerMockData = generateProducer()
+    const producerCropMockData = Array.from(
+      { length: faker.number.int({ min: 1, max: 5 }) },
+      generateCrop,
+    )
+
+    // Act
+    const producerCropInstance = producerCropMockData.map(
+      (crop) => new ProducerCrop(crop),
+    )
+    const producerInstance = new Producer({
+      ...producerMockData,
+      producerCrops: producerCropInstance,
+      cultivableAreaHectares: producerMockData.totalAreaHectares + 1,
+    })
+
+    // Assert
+    expect(producerInstance).toThrow('Invalid area distribution')
+  })
+
+  it.todo('should be able to delete a producer')
+  it.todo('should be able to edit a producer')
+  it.todo('should be able to read a producer')
+  it.todo('should be able to grow more than one crop in the farm')
+  it.todo('should be able to display amount of farms in quantity')
+  it.todo('should be able to display amount of farms in hectares - total area')
+  it.todo('should be able to display amount of farms by state')
+  it.todo('should be able to display amount of farms by crop')
+  it.todo(
+    'should be able to display amount of land usage - cultivable + vegetation',
+  )
 })
