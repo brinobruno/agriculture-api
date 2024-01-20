5bbda967e477aa21721a38f5aaa21000377d2314
Bruno Corrêa <brunosantos6ft@gmail.com>
2024-01-20 19:33:10 -0300

    chore: Define routes to be done

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -1,5 +1,13 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export const dashboardController = {
-  async getSomething(_request: FastifyRequest, reply: FastifyReply) {},
+  async getFarmsTotalQuantity(_request: FastifyRequest, reply: FastifyReply) {},
+
+  async getFarmsTotalHectares(_request: FastifyRequest, reply: FastifyReply) {},
+
+  async getFarmsByState(_request: FastifyRequest, reply: FastifyReply) {},
+
+  async getFarmsByCulture(_request: FastifyRequest, reply: FastifyReply) {},
+
+  async getSoilUsageRatio(_request: FastifyRequest, reply: FastifyReply) {},
 }
--- src/modules/dashboard/dashboard.routes.ts
+++ src/modules/dashboard/dashboard.routes.ts
@@ -2,5 +2,9 @@ import { FastifyInstance } from 'fastify'
 import { dashboardController } from './dashboard.controller'
 
 export async function dashboardRoutes(app: FastifyInstance) {
-  app.get('/', dashboardController.getSomething)
+  app.get('/', dashboardController.getFarmsTotalQuantity)
+  app.get('/', dashboardController.getFarmsTotalHectares)
+  app.get('/', dashboardController.getFarmsByState)
+  app.get('/', dashboardController.getFarmsByCulture)
+  app.get('/', dashboardController.getSoilUsageRatio)
 }
