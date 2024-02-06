05dc7c67c27426ca7c2a07125bb58d5112bde5f8
Bruno Correa <brunosantos6ft@gmail.com>
2024-02-06 16:28:28 -0300

    refactor: Use cpf/cnpj lib instead of random

--- src/shared/mockCpfCnpj.ts
+++ src/shared/mockCpfCnpj.ts
@@ -1,13 +1,8 @@
-const generateRandomNumber = (length: number) => {
-  const min = 10 ** (length - 1)
-  const max = 10 ** length - 1
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+import { cnpj, cpf } from 'cpf-cnpj-validator'
 
 export const mockCpfCnpj = () => {
   const isCpf = Math.random() < 0.5 // 50% chance of generating CPF
 
-  if (isCpf) return generateRandomNumber(11).toString() // CPF
-
-  return generateRandomNumber(14).toString() // CNPJ
+  if (isCpf) return cpf.generate()
+  return cnpj.generate()
 }
